e85774cce97282d94c058345d58a106ec5e03a07
Damian Molinski <tedeer@gmail.com>
2021-03-16 16:56:47 +0100

    Added class for c-btn--primary

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
-var gulp = require('gulp');
-var gulpSass = require('gulp-sass');
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
 
-gulp.task('buildcss', function(){
-  return gulp.src('./dev-assets/style.scss')
+gulp.task('buildcss', () => gulp.src('./dev-assets/style.scss')
   .pipe(gulpSass())
-  .pipe(gulp.dest('./prod-assets'));
-});
+  .pipe(gulp.dest('./prod-assets')));
 
-gulp.task('watch', function(){
+gulp.task('watch', () => {
   // gulp.watch('./dev-assets/**/*.scss', ['buildcss']);
   gulp.watch('./dev-assets/**/*.scss', gulp.series('buildcss'));
 });
